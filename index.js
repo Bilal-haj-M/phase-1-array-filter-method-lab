1d7870423c4e657e9a2cf5f794b9f53099ab7064
Bilal-hajM <bblal1003@gmail.com>
2023-04-29 02:02:22 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers , quary) {
+    return drivers.filter(driver => driver.toLowerCase() === quary.toLowerCase());
+}
+
+function fuzzyMatch(drivers , quary) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(quary.toLowerCase()))
+}
+
+function matchName(drivers , quary) {
+    return drivers.filter(driver => driver.name === quary)
+}
